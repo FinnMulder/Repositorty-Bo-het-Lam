12a85fe536b1d2f2cab4850d52f3f546110b6b68
FinnMulder <70947163+FinnMulder@users.noreply.github.com>
2021-01-13 15:25:17 +0100

    Add files via upload

---
+++ quiz simon en finn/quiz1.js
@@ -0,0 +1,77 @@
+const quiz1 =
+{
+    "quizID": "Museum 1",
+    "quizMetaData":
+    {
+        "title": "Quiz Simon en Finn",
+        "imageURI": "https://www.lamlisse.nl/wp-content/uploads/2018/03/LAM-Lisse-logo-white.svg"
+
+    },
+    "quizContent":		
+		[
+			{
+				"question": "Zin in de quiz?",
+				"answers": [{
+						"answer": "Ja",
+						"feedback": true
+					},
+					{
+						"answer": "Nee",
+						"feedback": false
+					},
+					{
+						"answer": "",
+						"feedback": false
+					},
+					{
+						"answer": "",
+						"feedback": false
+					},
+					{
+						"answer": "",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "Wat is LAM voor museum?",
+				"answers": [{
+						"answer": "Street art",
+						"feedback": false
+					},
+					{
+						"answer": "Food art",
+						"feedback": true
+					},
+					{
+						"answer": "Colorfull art",
+						"feedback": false
+					},
+					{
+						"answer": "Kitsch art",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "Wie is de oprichter van het Museum: ?",
+				"answers": [{
+						"answer": "Sem jansen",
+						"feedback": false
+					},
+					{
+						"answer": "Faas de jong",
+						"feedback": false
+					},
+					{
+						"answer": "Dirk ten have",
+						"feedback": false
+					},
+					{
+						"answer": "Jan van den broek",
+						"feedback": true
+					}
+				]
+			}
+		]
+	}
